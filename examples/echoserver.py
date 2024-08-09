6a676fac75e20cf8b73eaa0625f3c00d51729bbc
Martin Alge <martin@alge.se>
2024-08-09 13:56:40 +0200

    Add some examples

---
+++ examples/echoserver.py
@@ -0,0 +1,4 @@
+import autolib
+
+
+autolib.runHTTPServerThatReturnsTheSpecifiedText(host="0.0.0.0", port=8080, text="Hello World")
---
+++ examples/timeAPI.py
@@ -0,0 +1,4 @@
+import autolib
+
+
+autolib.runHTTPAPIreturningcurrenttimeinUTCplus3(host="0.0.0.0", port=8081)
