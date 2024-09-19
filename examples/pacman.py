6a4231d3feb6b7e3db55efcb5ea7d36ae9c386e4
Martin Alge <martin@alge.se>
2024-09-19 16:01:14 +0200

    pacman example

---
+++ examples/pacman.py
@@ -0,0 +1,4 @@
+import autolib
+
+autolib.usercontext = "Generate the following game, including ghosts and abillity to pick up coins, and a score counter"
+autolib.runPacmanWithASCIIGraphicsAndArrowKeysAsInput()
